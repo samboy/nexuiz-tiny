a2460592df5503d6cc241de85d4376f54f42aeba
Sam Trenholme <strenholme.usenet@gmail.com>
2020-05-06 17:43:16 -0700

    texture.finder.sh: Try to find missing textures in all seven maps.
    Somewhat related to https://github.com/samboy/nexuiz-tiny/issues/1

--- texture.finder.sh
+++ texture.finder.sh
@@ -2,8 +2,17 @@
 
 # This script helps us find out which textures a given .bsp file needs
 
-cd data/maps
-for a in *bsp ; do 
+for a in data/maps/*bsp GPLv3-data/maps/*bsp non-opensource/Aerowalk/maps/*bsp 
+do
+	DIR=${a%%/*}
+	# Only the Graphite map is GPLv3+; the textures are GPLv2+
+	if [ "$DIR" == "GPLv3-data" ] ; then
+		DIR="data"
+	fi
+	# The Aerowalk textures are in a sub-directory
+	if [ "$DIR" == "non-opensource" ] ; then
+		DIR="non-opensource/Aerowalk"
+	fi
 	strings $a | grep textures | awk '
-		{sub(/^[^t]+/,"");print $0 " '$a'"}' 
+		{sub(/^[^t]+/,"");print "'$DIR'/" $0 " '$a'"}' 
 done | sort
